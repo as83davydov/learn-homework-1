595a911964f660f295edbea1dce1a7512d2b0f25
Alexandr Davydov <iamdavydov@gmail.com>
2020-09-20 14:56:52 +0300

    Исключение KeyboardInterrupt

--- 6_exception1.py
+++ 6_exception1.py
@@ -10,11 +10,20 @@
     
 """
 
-def hello_user():
-    """
-    Замените pass на ваш код
-    """
-    pass
-    
+def greeting ():
+    while True:
+        try:
+            hello_user = input('Как дела?: ')
+        except KeyboardInterrupt:
+            print('Пока')
+        if hello_user == 'Хорошо':
+            print('Супер')
+            break
+        else:
+            print('А почему {}?'.format(hello_user))
+
 if __name__ == "__main__":
-    hello_user()
+    check_def = greeting()
+    print (check_def)
+
+

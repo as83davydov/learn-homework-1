bd730fc7b585115f537894df81b7c25236efaa89
Alexandr Davydov <iamdavydov@gmail.com>
2020-09-21 23:44:35 +0300

    Вопрос_ответ

--- 5_while2.py
+++ 5_while2.py
@@ -15,13 +15,16 @@
     
 """
 
-questions_and_answers = {}
+questions_and_answers = {"Как дела?": "Хорошо!", "Что делаешь?": "Программирую", "А теперь?": "Пью чай"}
 
-def ask_user(answers_dict):
-    """
-    Замените pass на ваш код
-    """
-    pass
+def ask_user():
+    while True:
+        hello_user = input('Пользователь: ')    
+        if hello_user in questions_and_answers:
+          print('Программа: {}'.format(questions_and_answers[hello_user]))
+        else:
+          hello_user
     
 if __name__ == "__main__":
-    ask_user(questions_and_answers)
+    convers = ask_user()
+    print (convers)

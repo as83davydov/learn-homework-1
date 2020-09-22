0455b848278094e850303515b7bdadace553591c
Alexandr Davydov <iamdavydov@gmail.com>
2020-09-23 00:10:41 +0300

    Обработка исключений

--- 7_exception2.py
+++ 7_exception2.py
@@ -13,16 +13,29 @@
     
 """
 
-def discounted(price, discount, max_discount=20)
-    """
-    Замените pass на ваш код
-    """
-    pass
+def discounted(price, discount, max_discount=20):
+    try:
+        price = float(price)
+        discount = abs(float(discount))
+        max_discount = int(max_discount)
+        if max_discount > 99:
+            raise ValueError('Слишком большая максимальная скидка')
+        if discount >= max_discount:
+            return price
+        else:
+            return price - (price * discount / 100)
+
+    except (ValueError, NameError):
+        print('Программа принимает на вход число')
+    except TypeError:
+        print('Программа не поддерживает данный тип данных')
     
 if __name__ == "__main__":
+    print(discounted([], 2))
     print(discounted(100, 2))
     print(discounted(100, "3"))
     print(discounted("100", "4.5"))
     print(discounted("five", 5))
     print(discounted("сто", "десять"))
     print(discounted(100.0, 5, "10"))
+    print(discounted(100.0, 5, 100))

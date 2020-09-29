58a8eb830ed1c049a605348408001ab3f073c953
Alexandr Davydov <iamdavydov@gmail.com>
2020-09-29 23:30:06 +0300

    В боте реализован модуль Ephem 2мя способами, через команду и без

--- 8_ephem_bot.py
+++ 8_ephem_bot.py
@@ -14,7 +14,11 @@
 """
 import logging
 
+import ephem
+
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+import settings
 
 logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
@@ -22,10 +26,10 @@ logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
 
 
 PROXY = {
-    'proxy_url': 'socks5://t1.learn.python.ru:1080',
+    'proxy_url': settings.PROXY_URL,
     'urllib3_proxy_kwargs': {
-        'username': 'learn',
-        'password': 'python'
+        'username': settings.PROXY_USERNAME,
+        'password': settings.PROXY_PASSWORD
     }
 }
 
@@ -39,16 +43,66 @@ def greet_user(update, context):
 def talk_to_me(update, context):
     user_text = update.message.text
     print(user_text)
-    update.message.reply_text(text)
+    update.message.reply_text(user_text)
+
+
+def planet_user(update, context): #функция показывает в каком созвездии планета без команды
+    date_planet = '2121/03/05'
+   
+    if update.message.text == 'mars':
+        planet = ephem.Mars(date_planet)        
+    elif update.message.text == 'venus':
+        planet = ephem.Venus(date_planet)
+    elif update.message.text == 'jupiter':
+        planet = ephem.Jupiter(date_planet)
+    elif update.message.text == 'earth':
+        planet = ephem.Earth(date_planet)      
+    else:
+      update.message.reply_text('что-то пошло не так')
+      return
+
+    const_planet = ephem.constellation(planet)
+    update.message.reply_text(f'{date_planet} Планета {update.message.text.upper()} в созвездии {const_planet[-1]}')
+
+  
+def get_planet(update, context): #функция показывает в каком созвездии планета с командой /planet
+    date_user = '2121/03/05' #input('Введите дату в формате год/месяц/день: ')
+
+    if context.args[0] == 'mars':
+        planetname = ephem.Mars(date_user)
+    elif context.args[0] == 'venus':
+        planetname = ephem.Venus(date_user)
+    elif context.args[0] == 'jupiter':
+        planetname = ephem.Jupiter(date_user)
+    elif context.args[0] == 'earth':
+        planetname = ephem.Earth(date_user)
+    elif context.args[0] == 'moon':
+        planetname = ephem.Moon(date_user)
+    elif context.args[0] == 'sun':
+        planetname = ephem.Sun(date_user)
+    elif context.args[0] == 'uranus':
+        planetname = ephem.Uranus(date_user)
+    elif context.args[0] == 'pluto':
+        planetname = ephem.Pluto(date_user)
+    else:
+        update.message.reply_text('ошибочка вышла')
+        return
+
+    constel_planet = ephem.constellation(planetname)
+    update.message.reply_text(f'{date_user} Планета {context.args[0].upper()} в созвездии {constel_planet[-1]}')
 
 
 def main():
-    mybot = Updater("КЛЮЧ, КОТОРЫЙ НАМ ВЫДАЛ BotFather", request_kwargs=PROXY, use_context=True)
+    mybot = Updater(settings.API_KEY, request_kwargs=PROXY, use_context=True)
 
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(CommandHandler("planet", get_planet))
+    dp.add_handler(MessageHandler(Filters.text, planet_user))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 
+
+    logging.info('Бот стартовал')
     mybot.start_polling()
     mybot.idle()
 
